c0e6e9407ea2f4b824bf0c2e4a54e52eb1026492
Bong <mjbond_bong2@yahoo.com>
2020-06-28 19:27:16 +0800

    add pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,44 @@
 class WelcomeController < ApplicationController
+
+	def video
+	end
+
+	def esport  
+  	end
+
+	def tank_info  
+  	end
+
+  	def tank_roi  
+  	end
+
+  	def assasin_info  
+  	end
+
+  	def assasin_roi  
+  	end
+
+  	def fighter_info  
+  	end
+
+  	def fighter_roi  
+  	end
+
+  	def mage_info  
+  	end
+
+  	def mage_roi  
+  	end
+
+  	def support_info  
+  	end
+
+  	def support_roi  
+  	end
+
+  	def mm_info  
+  	end
+
+  	def mm_roi  
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   get 'tank_info' => 'welcome#tank_info'
+   get 'tank_roi' => 'welcome#tank_roi'
+
+   get 'mm_info' => 'welcome#mm_info'
+   get 'mm_roi' => 'welcome#mm_roi'
+
+   get 'assasin_info' => 'welcome#assasin_info'
+   get 'assasin_roi' => 'welcome#assasin_roi'
+
+   get 'fighter_info' => 'welcome#fighter_info'
+   get 'fighter_roi' => 'welcome#fighter_roi'
+
+   get 'mage_info' => 'welcome#mage_info'
+   get 'mage_roi' => 'welcome#mage_roi'
+
+   get 'support_info' => 'welcome#support_info'
+   get 'support_roi' => 'welcome#support_roi'
+
+   get 'video' => 'welcome#video'
+   get 'esport' => 'welcome#esport'
 end
